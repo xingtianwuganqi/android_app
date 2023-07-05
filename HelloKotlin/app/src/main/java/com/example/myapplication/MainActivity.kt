57be872fab30a4622481a6dbe63237b5e17e2a69
景军 <rxswift@126.com>
2023-07-05 23:43:16 +0800

    fix

--- HelloKotlin/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ HelloKotlin/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultCallback
@@ -16,6 +17,8 @@ class MainActivity : AppCompatActivity() {
     private val launcherCallback = ActivityResultCallback<ActivityResult> { result ->
         val code = result.resultCode
         val data = result.data
+        println(code)
+        println(data)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,10 +28,10 @@ class MainActivity : AppCompatActivity() {
         resultLauncher = registerForActivityResult(
             ActivityResultContracts.StartActivityForResult(),
             launcherCallback)
-
+        println("哈哈哈")
         val button: Button = findViewById(R.id.button)
         button.setOnClickListener {
-            val intent = Intent(this, secondActivity::class.java)
+            val intent = Intent(this, SecondActivity::class.java)
             intent.putExtra("title","value")
             intent.putExtra("date","23年7月1日")
 //            resultLauncher.launch(intent)
--- HelloKotlin/app/src/main/java/com/example/myapplication/secondActivity.kt
+++ HelloKotlin/app/src/main/java/com/example/myapplication/secondActivity.kt
@@ -1,20 +1,28 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 
-class secondActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
-
-        print("哈哈哈")
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            val intent = Intent()
+            intent.putExtra("name","secondActivity")
+            setResult(RESULT_OK,intent)
+            finish()
+        }
+        println("哈哈哈2")
         var extIntent = intent.getStringExtra("title")
-        Log.d("ListActivity",extIntent ?: "")
+        Log.d("SecondActivity",extIntent ?: "")
         var date = intent.getStringExtra("date")
-        Log.d("listac",date ?: "")
-        print(extIntent)
-        print(date)
+        Log.d("SecondActivity",date ?: "")
+        println(extIntent)
+        println(date)
     }
 }
