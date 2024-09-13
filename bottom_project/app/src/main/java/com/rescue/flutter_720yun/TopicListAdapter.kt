3a8e0f3e71e028ccb2230501bed9ad5fdae1eb65
景军 <rxswift@126.com>
2024-09-13 23:38:51 +0800

    fix

--- bottom_project/app/src/main/java/com/rescue/flutter_720yun/TopicListAdapter.kt
+++ bottom_project/app/src/main/java/com/rescue/flutter_720yun/TopicListAdapter.kt
@@ -1,6 +1,6 @@
 package com.rescue.flutter_720yun
 
-import android.util.Log
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,6 +14,7 @@ class TopicListAdapter(private val list: List<HomeListModel>): RecyclerView.Adap
     inner class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
         val name = view.findViewById<TextView>(R.id.nick_name)
         var imgView = view.findViewById<ImageView>(R.id.head_img)
+        val content = view.findViewById<TextView>(R.id.content)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -29,7 +30,10 @@ class TopicListAdapter(private val list: List<HomeListModel>): RecyclerView.Adap
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = list[position]
         holder.name.text = item.userInfo.username
+        holder.name.textAlignment = View.TEXT_ALIGNMENT_TEXT_START
         val imgStr = "http://img.rxswift.cn/${item.userInfo.avator}"
-        Picasso.get().load(imgStr).into(holder.imgView);
+        Picasso.get().load(imgStr).transform(CircleTransformation()).into(holder.imgView)
+        holder.imgView.scaleType = ImageView.ScaleType.CENTER_CROP
+        holder.content.text = item.content
     }
 }
